2c986b4e7825a040d6c9f6294eb9c37539c7afb8
heretodesign <senwelo.works@gmail.com>
2020-05-28 15:18:46 +0800

    crud operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  # resources :articles, only: [:show, :index]
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
